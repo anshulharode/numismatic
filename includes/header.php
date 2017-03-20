93b8c7625595cb56ab2d1c462bdb474bdda29d9c
an0nh4x0r <swappp7singh@gmail.com>
2017-03-20 22:41:44 +0530

    sidenav ready

--- includes/header.php
+++ includes/header.php
@@ -6,19 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Numismatic</title>
     <link rel="stylesheet" type="text/css" href="http://bootswatch.com/darkly/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
 
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a class="navbar-brand" href="#">Numismatic</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
@@ -26,7 +30,8 @@
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                       aria-expanded="false">Dropdown <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="#">Action</a></li>
                         <li><a href="#">Another action</a></li>
@@ -39,10 +44,10 @@
                 </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="../login.php">Login</a></li>
+                <li><a href="../login.php">Sign In</a></li>
                 <li><a href="../signup.php">Sign Up</a></li>
-                <li class="active"><a href="../">Fixed top <span class="sr-only">(current)</span></a></li>
             </ul>
-        </div><!--/.nav-collapse -->
+        </div>
+        <!--/.nav-collapse -->
     </div>
 </nav>
--- index.php
+++ index.php
@@ -2,6 +2,39 @@
   include_once "includes/header.php";
 ?>
 
+
+<div class="row">
+    <ul class="list-group col-md-3" id="sidenav">
+        <li class="list-group-item menu"> <i class="fa fa-tags" aria-hidden="true"></i> Categories</li>
+        <li class="list-group-item">
+            <a href="#">Coins</a>
+        </li>
+        <li class="list-group-item">
+            <a href="#">Paper Money</a>
+        </li>
+        <li class="list-group-item">
+            <a href="#">Antiques</a>
+        </li>
+        <li class="list-group-item menu"><i class="fa fa-globe" aria-hidden="true"></i> Country</li>
+        <li class="list-group-item">
+            <a href="#">India</a>
+        </li>
+        <li class="list-group-item">
+            <a href="#">UAE</a>
+        </li>
+        <li class="list-group-item">
+            <a href="#">Nepal</a>
+        </li>
+        <li class="list-group-item">
+            <a href="#">America</a>
+        </li>
+        <li class="list-group-item">
+            <a href="#">Other</a>
+        </li>
+    </ul>
+
+</div>
+
 <?php
 include_once "includes/footer.php"
 ?>
