fbb8ac8d8fa0f6e6eb4df539f76ca7870eb74c5a
Swapnil Singh <swappp7singh@gmail.com>
2017-03-20 12:39:50 +0530

    included bootswatch

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Numismatic</title>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="http://bootswatch.com/darkly/bootstrap.min.css">
 </head>
 <body>
 
